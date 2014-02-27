205b54b85c6fc0374f5fc2f1e16be4f87b96975f
Camilo Pereira <camilomorpheu@gmail.com>
2014-02-27 12:28:14 -0300

    Added parameter checking

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php $ssl_server = "https://www70.secrel.com.br/eao/"; if(isset($_SERVER) && $_SERVER['HTTPS']): echo 1; else: header("Location: " . $ssl_server . substr( $_SERVER["SCRIPT_NAME"],5)  . "?". $_SERVER["QUERY_STRING"] ); endif; ?>
+<?php $ssl_server = "https://www70.secrel.com.br/eao/"; $has_parameters = (strlen($_SERVER['QUERY_STRING']) > 0) ? "?" : "";  if(isset($_SERVER) && $_SERVER['HTTPS']): echo 1; else: header("Location: " . $ssl_server . substr( $_SERVER["SCRIPT_NAME"],5) . $has_parameters . $_SERVER["QUERY_STRING"] ); endif; ?>
